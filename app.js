5d70beec97ee6be55c07cfbcbdd9d7835bc40f47
muhammadzaid777 <muhammadzaidabbasi123@gmail.com>
2026-08-20 13:46:47 +0500

    JS ASSIGNMENT 1

---
+++ app.js
@@ -0,0 +1,249 @@
+// ..........CHAPTER 1..........
+
+// 1
+// alert("hello")
+
+// 2
+// alert("Error! Please enter a valid password")
+
+// 3
+// alert("Welsome to JS land \n Happy coding")
+
+//4
+// alert("Welsome to JS land")
+// alert("Happy coding")
+
+//5
+
+// ..........CHAPTER 2..........
+
+
+// 1
+// var username =;
+
+// 2
+// var MyName = "Muhammad Ahmed"
+
+// 3
+// var massege = "Hello World"
+// alert(massege)
+
+// 4
+// var Name = "Muhammad Ahmed"
+// var Age = "16 years"
+// var Course = "Web development"
+
+// alert(Name)
+// alert(Age)
+// alert(Course)
+
+// 5
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// 6
+// var email = "muhammadahmed777@gmail.com";
+// alert("My Email adress is "+ email)
+
+// 7
+// var Book = "A smarter way to learn JavaScript"
+// alert("Im learning jawa script from Book " + Book )
+
+// 8
+// document.write("yeah I can write HTML content throug Java script")
+
+// 9
+// var string = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(string)
+
+// CHAPTER 3
+// 1
+// var Age = "16 years"
+// alert(Age)
+
+// 2
+// var times = 14
+// alert("you have visited this site "+ times +" times")
+
+// 3
+// var MyBirth = 2010
+// document.write("My birth year is " + MyBirth)
+
+// 4
+// var visitor = "John Doe"
+// var product = "XYZ clothing store"
+// var quantity = "5 t Shirts"
+// alert(visitor + " ordered " + quantity + " from " + product)
+
+// ..........CHAPTER 4..........
+
+// 1
+// var MyName = "Muhammad Ahmed ", Age = " 16 years ", Course = " Web Development"
+// alert(MyName + Age + Course)
+
+// 2
+
+// LEGAL
+// var name;
+// var age;
+// var myName;
+// var user_1;
+// var $price;
+
+// ILLEGAL
+// var 1name;
+// var my-name;
+// var var;
+// var my name;
+// var @email;
+
+// 3
+// document.write("<h1>Rules for naming JS variables</h1>");
+
+// document.write("Variable names can only contain letters, numbers, $ and _.<br>");
+// document.write("Variables must begin with a $name, name or Name.<br>");
+// document.write("Variable names are case sensitive<br>");
+// document.write("Variable names should not be JS Keywords<br>");
+
+// 4
+
+// ..........CHAPTER 5.........
+
+// 1
+// var value1 = 3, value2 = 5
+// document.write("sum of 3 and 5 is " + (value1 + value2))
+
+// 2
+// var value1 = 3, value2 = 5
+// document.write("sum of 3 and 5 is " + (value1 - value2))
+
+// var value1 = 3, value2 = 5
+// document.write("sum of 3 and 5 is " + (value1 * value2))
+
+// var value1 = 3, value2 = 5
+// document.write("sum of 3 and 5 is " + (value1 / value2))
+
+// var value1 = 3, value2 = 5
+// document.write("sum of 3 and 5 is " + (value1 % value2))
+
+// 3
+// var num =
+
+// document.write("Value after variable declaration is: ?? <br>")
+// num = 5
+// document.write("Initial value: " + num + "<br>")
+// num = ++num
+
+// document.write("Value after increment is: " + num + "<br>")
+// num = num + 7
+
+// document.write("Valu after addition is: " + num + "<br>")
+// --num
+
+// document.write("Value after decrement is: " + num + "<br>")
+
+// num = num % 3
+// document.write("The remainder is: "+ num)
+
+// 4
+// var ticket = 600
+// var totalcost = ticket * 5
+// document.write("Total cost to buy 5 tickets to a movie is " + totalcost +"PKR")
+
+// 5
+// var table = 5
+// document.write("5 x 1 = " +table     + "<br>")
+// document.write("5 x 2 = " +table *2  + "<br>")
+// document.write("5 x 3 = " +table *3  + "<br>")
+// document.write("5 x 4 = " +table *4  + "<br>")
+// document.write("5 x 5 = " +table *5  + "<br>")
+// document.write("5 x 6 = " +table *6  + "<br>")
+// document.write("5 x 7 = " +table *7  + "<br>")
+// document.write("5 x 8 = " +table *8  + "<br>")
+// document.write("5 x 9 = " +table *9  + "<br>")
+// document.write("5 x 10 = " +table *10+ "<br>")
+
+// 6
+
+// var celsius = 100;
+// var fahrenheitFromC = (celsius * 9 / 5) + 32;
+// document.write(celsius + "°C is " + fahrenheitFromC + "°F<br>");
+
+// var fahrenheit = 98.6;
+// var celsiusFromF = (fahrenheit - 32) * 5 / 9;
+// document.write(fahrenheit + "°F is " + celsiusFromF + "°C");
+
+// 7
+
+// var item1 = 650 ,item1quantity = 3
+// document.write("Price of item one is " +item1 + "<br>")
+// document.write("Price of item one is " +item1quantity +"<br>")
+
+// var item2 = 100 ,item2quantity = 7
+// document.write("Price of item one is " +item2 + "<br>")
+// document.write("Price of item one is " +item2quantity +"<br>")
+
+// var shipingcharges = 100
+// document.write("Shipping charges are " +shipingcharges +"<br>")
+
+// item1 = item1 * item1quantity
+// item2  = item2 * item2quantity
+
+// document.write("Total cost fo your order is " + (item1 + item2 + shipingcharges))
+
+//8
+// var total_marks = 980, marks_obtained = 804
+// document.write("Total Marks " +total_marks+"<br>")
+// document.write("Marks Obtained " +marks_obtained +"<br>")
+
+// percentage = marks_obtained / total_marks
+
+// document.write("Percentage " +percentage)
+
+// 9
+// document.write("<h1>Currency in PKR</h1>")
+
+// var OneUsDollar = 104.80, OneSaudiRiyal = 28
+// OneUsDollar = OneUsDollar * 10, OneSaudiRiyal = OneSaudiRiyal * 25
+// var pkr =  OneUsDollar + OneSaudiRiyal
+
+
+// document.write("The Currency in PKR " + pkr)
+
+// 10
+// var value = 10 + 5 * 10 / 2
+// document.write("Calculation: " + value)
+
+// 11
+// document.write("<h1>Age Calculation</h1>")
+
+// var CurrentYear = 2026, BirthYear = 2010
+// document.write("Current Year: " +CurrentYear + "<br>")
+// document.write("Birth Year: " +BirthYear + "<br>")
+// document.write("Your age is: " +(CurrentYear - BirthYear))
+
+// 12
+// var radius = 20;
+// var pi = 3.142;
+// var circumference = 2 * pi * radius;
+// var area = pi * radius * radius;
+
+
+// document.write("<h1>The Geometrizer</h1>");
+// document.write("Radius of a circle: " + radius + "<br>");
+// document.write("The circumference is: " + circumference + "<br>");
+// document.write("The area is: " + area);
+
+//13
+// var favSnack = "chips";
+// var currentAge = 14;
+// var maxAge = 65;
+// var amountPerDay = 3;
+// var totalNeeded = (maxAge - currentAge) * 365 * amountPerDay;
+
+
+// document.write("<h1>The Lifetime Supply Calculator</h1>");
+// document.write("Favourite Snack: " + favSnack + "<br>");
+// document.write("Current age: " + currentAge + "<br>");
+// document.write("Estimated Maximum Age: " + maxAge + "<br>");
+// document.write("Amount of snacks per day: " + amountPerDay + "<br>");
+// document.write("You will need " + totalNeeded + " " + favSnack + " to last you until the ripe old age of " + maxAge);
